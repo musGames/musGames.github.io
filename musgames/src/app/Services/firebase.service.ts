8997355a0ee0d28f122e327574524f22ee6e3830
Usama Abbas <ua@praktikcenter.lan>
2026-04-21 10:44:16 +0200

    Besked

--- musgames/src/app/Services/firebase.service.ts
+++ musgames/src/app/Services/firebase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { getDatabase, Database } from 'firebase/database';
+import { getDatabase, Database, ref, get } from 'firebase/database';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 import { environment } from '../../environment/environment';
 
@@ -19,5 +19,27 @@ export class FirebaseService {
     this.auth = getAuth(this.app);
     this.db = getDatabase(this.app);
     this.storage = getStorage(this.app);
+  }
+
+  // Bruges i din component
+  getDatabase(): Database {
+    return this.db;
+  }
+
+  // Henter highscores for valgt spil
+  getHighscoresForGame(gameId: string): Promise<any[]> {
+    const highscoresRef = ref(this.db, 'highscores/');
+
+    return get(highscoresRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+
+        return Object.keys(data)
+          .map(key => ({ id: key, ...data[key] }))
+          .filter(score => score.games_Id === gameId);
+      }
+
+      return [];
+    });
   }
 }
---
+++ musgames/src/app/leaderboard/leaderboard.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../Services/firebase.service';
+import { get, ref, onValue} from 'firebase/database';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-leaderboard',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './leaderboard.html',
+  styleUrl: './leaderboard.css',
+})
+export class Leaderboard implements onInit {
+
+  highscores: any[] = [];
+  games: any [] = [];
+  selectedGameId: string = "";
+
+  constructor(private firebaseService: FirebaseService) {}
+  
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  async loadGames() {
+
+    const gamesRef = ref(this.firebaseService.getDatabase(), 'games/');
+    const snapshot = await get(gamesRef);
+
+    if (snapshot.exists()) {
+      this.games = Object.keys(snapshot.val()).map(key => {
+        return { id: key, name: snapshot.val()[key].title };
+      });
+    } else {
+      console.log("Ingen spil fundet i firebase");
+    }
+  }
+
+  async loadHighscores() {
+    if (this.selectedGameId) {
+      this.firebaseService.getHighscoresForGame(this.selectedGameId)
+      .then(this.highscores => {
+        this.highscores = this.highscores.sort((a, b) => b.score - a.score);
+      });
+    }
+  }
+
+  listenForHighscoreUpdates() {
+    if (this.selectedGameId) {
+      const highscoresRef = ref (this.firebaseService.getDatabase(), 'highscores/');
+
+      onValue(highscoresRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const highscoresData = snapshot.val();
+          this.highscores = [];
+
+          for (let id in highscoresData) {
+            const data = highscoresData[id];
+
+            if (data.games_Id === this.selectedGameId) {
+              this.highscores.push({
+                displayName: data.displayName,
+                gameTitle: data.gameTitle,
+                score: data.score
+              });
+            }
+          }
+
+          this.highscores.sort((a, b) => b.score - a.score);
+        }
+      });
+    }
+  }
+
+}
